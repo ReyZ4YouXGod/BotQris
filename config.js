b708fc313724170d78ce1b21cf6009617de62d72
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-05-12 20:52:40 +0700

    Add config.js for bot and API settings
    Initial configuration file for bot settings and API.

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // --- BOT SETTINGS ---
+    botToken: '7805124868:AAFZtImH0qfvyddaX2ba4NwXEPy55k6n04I',
+    adminUsername: '@ReyCloudDev',
+    channelLogId: '-1002597815366', // GANTI DENGAN ID CHANNEL KAMU
+
+    // --- PAKASIR API SETTINGS ---
+    pakasir: {
+        project: "reyclouddev",
+        api_key: "9AwCqt0h99ArK0Jy7R5PYpP1FmdQ0SWN", // GANTI DENGAN API KEY PAKASIR KAMU
+        base_url: "https://app.pakasir.com/api"
+    },
+
+    // --- ASSETS ---
+    videoIntro: 'https://k.top4top.io/m_37841mg1e4.mp4',
+    
+    // --- MESSAGES ---
+    notes: 'System Bot Ini Hanya digunakan untuk deposit ReyCloud!'
+};
